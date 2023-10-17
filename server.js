7b652976bf8c5bde1665ae0b4ace88af5c343f86
mohsen mohamadian <mamaddnet@gmail.com>
2023-10-17 16:47:14 +0330

    connect to the DATABASE

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const app = require('./app');
+const app = require("./app");
+const mongoose = require("mongoose");
 
+const DB = process.env.DATABASE.replaceAll(
+  "<PASSWORD>",
+  process.env.DATABASE_PASS
+);
+console.log(DB);
 
-
-
-
+mongoose.connect(DB).then((con) => {
+  console.log("connect to the DB");
+});
 
 const PORT = process.env.PORT;
 
-
-app.listen(PORT, ()=>{
-    console.log("server listen on port "+ PORT);
-})
+app.listen(PORT, () => {
+  console.log("server listen on port " + PORT);
+});
